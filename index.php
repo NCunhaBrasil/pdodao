76a104ee51442097853194ff8acd5db46986b4fe
Anderson Cunha <ncunha@ncunha.com.br>
2017-08-03 13:47:29 -0300

    Metodos de Lista

--- index.php
+++ index.php
@@ -1,9 +1,24 @@
 ﻿<?php
 
 require_once("config.php");
+//Carrega apenas um usuario
+//$root = new Usuario();
+//22 é o id do usuario
+//$root->loadbyId(22);
+//echo $root;
 
-$root = new Usuario();
-$root->loadbyId(12);
-echo $root;
+//Carraga uma lista de usuarios
+//$lista = usuario::getList();
+//echo json_encode($lista); 
+
+//Carrega uma lista de usuarios buscando pelo login  <-- Não funcionando ainda
+//$search = Usuario::search("der");
+//echo json_encode($search);
+
+
+//Carrega um usuario usando login e senha
+$usuario = new Usuario();
+$usuario->login("Anderson","N Cunha");
+echo $usuario;
 
 ?>
